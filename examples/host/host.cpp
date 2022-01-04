b125988aebd06623cf370515e3db1a4eb536e14e
Lukas Berbuer <lukas.berbuer@gmail.com>
2022-01-04 01:55:37 +0100

    Strip host example

--- examples/host/host.cpp
+++ examples/host/host.cpp
@@ -1,36 +1,8 @@
-#include <dlfcn.h>
-#include <functional>
 #include <iostream>
 #include <stdexcept>
 
-#include "vamp/vamp.h"
-
-static void resetDllError() {
-    dlerror();
-}
-
-static void checkDllError() {
-    const char* error = dlerror();
-    if (error) {
-        throw std::runtime_error(error);
-    }
-}
+#include "rtvamp/hostsdk/PluginLoader.hpp"
 
 int main() {
-    void* handler = dlopen("example-plugin.so", RTLD_NOW);
-    if (!handler) {
-        throw std::runtime_error(dlerror());
-    }
-    resetDllError();
 
-    auto func = (const VampPluginDescriptor* (*)(unsigned int, unsigned int)) dlsym(handler, "vampGetPluginDescriptor");
-    checkDllError();
-
-    const VampPluginDescriptor* descriptor = func(2, 0);
-    if (!descriptor) {
-        throw std::runtime_error("No descriptor returned");
-    }
-    std::cout << "Got descriptor" << std::endl;
-
-    dlclose(handler);
 }
