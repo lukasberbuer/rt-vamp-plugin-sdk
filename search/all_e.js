605c490af7ae68214749a10872ea6772aafaa374
lukasberbuer <lukasberbuer@users.noreply.github.com>
2022-11-19 22:19:56 +0000

    deploy: d6b03a68bef4a737167c6726485a406797fedcd9

--- search/all_e.js
+++ search/all_e.js
@@ -7,5 +7,5 @@ var searchData=
   ['reset_85',['reset',['../classrtvamp_1_1hostsdk_1_1Plugin.html#af7463fe0536a5646b922a9701177e57c',1,'rtvamp::hostsdk::Plugin::reset()'],['../classrtvamp_1_1hostsdk_1_1PluginHostAdapter.html#a042beb0e81ab4303b3fbf45ee8cc587a',1,'rtvamp::hostsdk::PluginHostAdapter::reset()'],['../classrtvamp_1_1pluginsdk_1_1Plugin.html#a323cabf03e4613e2ac5f39241eb492df',1,'rtvamp::pluginsdk::Plugin::reset()']]],
   ['rtvamp_86',['rtvamp',['../namespacertvamp.html',1,'']]],
   ['rtvamp_5fentry_5fpoint_87',['RTVAMP_ENTRY_POINT',['../EntryPoint_8hpp.html#ab9e8a9880a2de6797b776d72d446ae98',1,'EntryPoint.hpp']]],
-  ['rtvamp_5fexport_5fentry_5fpoint_88',['RTVAMP_EXPORT_ENTRY_POINT',['../EntryPoint_8hpp.html#a824efb6020c6c3a47a4ee2f4040e0117',1,'EntryPoint.hpp']]]
+  ['rtvamp_5fexport_5ffunction_88',['RTVAMP_EXPORT_FUNCTION',['../EntryPoint_8hpp.html#a6a1321320507dae050728a7781adec81',1,'EntryPoint.hpp']]]
 ];
--- search/defines_0.js
+++ search/defines_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['rtvamp_5fentry_5fpoint_204',['RTVAMP_ENTRY_POINT',['../EntryPoint_8hpp.html#ab9e8a9880a2de6797b776d72d446ae98',1,'EntryPoint.hpp']]],
-  ['rtvamp_5fexport_5fentry_5fpoint_205',['RTVAMP_EXPORT_ENTRY_POINT',['../EntryPoint_8hpp.html#a824efb6020c6c3a47a4ee2f4040e0117',1,'EntryPoint.hpp']]]
+  ['rtvamp_5fexport_5ffunction_205',['RTVAMP_EXPORT_FUNCTION',['../EntryPoint_8hpp.html#a6a1321320507dae050728a7781adec81',1,'EntryPoint.hpp']]]
 ];
