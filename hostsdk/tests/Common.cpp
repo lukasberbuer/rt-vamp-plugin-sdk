a7f42ee01ac3f361d1826eb7b0366b305b694b44
Lukas Berbuer <lukas.berbuer@gmail.com>
2025-04-02 17:06:52 +0200

    build: configure search paths for test plugins

--- hostsdk/tests/Common.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-#include <cstdlib>
-
-TEST_CASE("VAMP_PATH environment variable") {
-    INFO("VAMP_PATH environment variable must be set to output directory to discover test plugins");
-    REQUIRE(std::getenv("VAMP_PATH"));
-}
