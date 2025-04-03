2cc12e58628073dd1f471088da9ebd1e7db297c1
lukasberbuer <lukasberbuer@users.noreply.github.com>
2025-04-03 15:05:45 +0000

    deploy: a229e9085a1c48fd39844d8f0fcbb797e00750a6

--- search/all_16.js
+++ search/all_16.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['_7eplugin_0',['~Plugin',['../classrtvamp_1_1hostsdk_1_1Plugin.html#ae57b2e1eee6556997849960444d3c2b1',1,'rtvamp::hostsdk::Plugin']]],
   ['_7epluginbase_1',['~PluginBase',['../classrtvamp_1_1pluginsdk_1_1PluginBase.html#a2a8fff9afed79cfeef0c4167f6f2d49c',1,'rtvamp::pluginsdk::PluginBase']]],
-  ['_7epluginhostadapter_2',['~PluginHostAdapter',['../classrtvamp_1_1hostsdk_1_1PluginHostAdapter.html#a4c791107e7181e709127429a238f67bf',1,'rtvamp::hostsdk::PluginHostAdapter']]]
+  ['_7epluginhostadapter_2',['~PluginHostAdapter',['../classrtvamp_1_1hostsdk_1_1PluginHostAdapter.html#a2580941def9ab6daca7736758ea4be6a',1,'rtvamp::hostsdk::PluginHostAdapter']]]
 ];
